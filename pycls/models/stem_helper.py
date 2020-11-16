7455ab7b84c4e241e8b6c70a90833baaed1e96d1
Universe <bigtree123433@gmail.com>
2020-11-16 21:44:33 +0900

    V1 dilation separation bn entire

--- pycls/models/stem_helper.py
+++ pycls/models/stem_helper.py
@@ -220,6 +220,36 @@ class ResStemEndstopDilationSeparationEntire(Module):
         return cx
 
 
+class ResStemEndstopDilationSeparationBnEntire(Module):
+    """ResNet stem for ImageNet: 7x7, BN, AF, MaxPool."""
+
+    def __init__(self, w_in, w_out):
+        super(ResStemEndstopDilationSeparationBnEntire, self).__init__()
+        self.conv = conv2d(w_in, w_out, 7, stride=2)
+        self.bn = norm2d(w_out)
+        self.af = activation()
+        self.e = EndstoppingDilation(w_out, w_out, 3, stride=1, groups=w_out)
+        self.e_bn = norm2d(w_out)
+        self.pool = pool2d(w_out, 3, stride=2)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.af(x)
+        x = self.e(x)
+        x = self.e_bn(x)
+        x = self.af(x)
+        x = self.pool(x)
+        return x
+
+    @staticmethod
+    def complexity(cx, w_in, w_out):
+        cx = conv2d_cx(cx, w_in, w_out, 7, stride=2)
+        cx = norm2d_cx(cx, w_out)
+        cx = pool2d_cx(cx, w_out, 3, stride=2)
+        return cx
+
+
 class ResStemEndstopDilationPreluSeparationEntire(Module):
     """ResNet stem for ImageNet: 7x7, BN, AF, MaxPool."""
 
