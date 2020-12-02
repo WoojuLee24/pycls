02de2b42ea29dca9a9fbbb7f3c9c5f43cd50e619
Universe <bigtree123433@gmail.com>
2020-12-02 20:27:02 +0900

    cifar10 group entire

--- pycls/models/stem_helper.py
+++ pycls/models/stem_helper.py
@@ -90,6 +90,33 @@ class ResStemCifarDivide5x5ConvDcEntire(Module):
         return cx
 
 
+class ResStemCifarDivide5x5GroupEntire(Module):
+    """ResNet stem for CIFAR: 3x3, BN, AF."""
+
+    def __init__(self, w_in, w_out):
+        super(ResStemCifarDivide5x5GroupEntire, self).__init__()
+        self.conv = conv2d(w_in, w_out, 3)
+        self.bn = norm2d(w_out)
+        self.e = EndstoppingDivide5x5(w_out, w_out*4, 5, stride=1, groups=w_out)
+        self.conv2 = conv2d(w_out*4, w_out, 1, stride=1, groups=w_out)
+        self.af = activation()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.af(x)
+        x = self.e(x)
+        x = self.af(x)
+        x = self.conv2(x)
+        return x
+
+    @staticmethod
+    def complexity(cx, w_in, w_out):
+        cx = conv2d_cx(cx, w_in, w_out, 3)
+        cx = norm2d_cx(cx, w_out)
+        return cx
+
+
 class ResStemCifarDivide5x5ConvOnlyDcEntire(Module):
     """ResNet stem for CIFAR: 3x3, BN, AF."""
 
