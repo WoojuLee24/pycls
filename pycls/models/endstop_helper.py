8f3b693545370a6173a965c4620ebe4709cb96d4
Universe <bigtree123433@gmail.com>
2020-12-03 20:17:37 +0900

    th3

--- pycls/models/endstop_helper.py
+++ pycls/models/endstop_helper.py
@@ -133,6 +133,80 @@ class EndstoppingDivide5x5(nn.Conv2d):
         return x
 
 
+class EndstoppingDoG5x5(nn.Conv2d):
+
+    """
+    End-stopping Divide kernel for solving aperture problem
+    Using relu function to learn center-surround suppression
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation=1, bias=False, groups=1):
+        super().__init__(in_channels, out_channels, kernel_size, stride=stride, padding=padding, dilation=dilation, bias=bias)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.groups = groups
+        self.padding = padding
+        self.replication_pad = nn.ReplicationPad2d(2)
+        self.param = self.get_param(self.in_channels, self.out_channels, self.kernel_size, self.groups)
+        self.center_threshold, self.surround_threshold = self.get_threshold_param(self.in_channels, self.out_channels, self.kernel_size, self.groups)
+
+    def get_param(self, in_channels, out_channels, kernel_size, groups):
+        param = torch.zeros([out_channels, in_channels//groups, kernel_size, kernel_size], dtype=torch.float, requires_grad=True)
+        param = param.cuda()
+        fan_out = kernel_size * kernel_size * out_channels
+        param.data.normal_(mean=0.0, std=np.sqrt(2.0 / fan_out))
+        # nn.init.kaiming_normal_(param, mode='fan_out', nonlinearity='relu')
+        return nn.Parameter(param)
+
+
+    def get_threshold_param(self, in_channels, out_channels, kernel_size, groups):
+
+        center = torch.tensor([[0, 0, 0, 0, 0],
+                               [0, 0.17, 0.49, 0.17, 0],
+                               [-0.18, 0.49, 1, 0.49, 0],
+                               [0, 0.17, 0.49, 0.17, 0],
+                               [0, 0, 0, 0, 0]], requires_grad=False).cuda()
+        center = center.repeat(out_channels, in_channels//groups, 1, 1)
+
+        surround = torch.tensor([[-0.27, -0.23, -0.18, -0.23, -0.27],
+                               [-0.23, 0, 0, 0, -0.23],
+                               [-0.18, 0, 0, 0, -0.18],
+                               [-0.23, 0, 0, 0, -0.23],
+                               [-0.27, -0.23, -0.18, -0.23, 0.27]], requires_grad=False).cuda()
+        surround = surround.repeat(out_channels, in_channels // groups, 1, 1)
+
+        return center, surround
+
+    def get_weight_5x5(self, param):
+        """
+        5x5 surround modulation
+        center: relu(x) + relu(-x)
+        surround: - relu(x) - relu(-x)
+        """
+        center = F.pad(param[:, :, 1:4, 1:4], (1, 1, 1, 1))
+        surround = param - center
+        surround = surround * 9/16
+        center = F.relu(center) + F.relu(-center)
+        surround = - F.relu(surround) - F.relu(-surround)
+        center = torch.max(center, self.center_threshold)
+        surround = torch.min(surround, self.surround_threshold)
+
+        weight = center + surround
+
+        return weight
+
+
+    def forward(self, x):
+        weight = self.get_weight_5x5(self.param)
+        x = self.replication_pad(x)
+        x = F.conv2d(x, weight, stride=self.stride, groups=self.groups)
+        # x = F.conv2d(x, weight, stride=self.stride, padding=self.padding, groups=self.groups)
+        return x
+
+
 class EndstoppingDilation(nn.Conv2d):
 
     """
