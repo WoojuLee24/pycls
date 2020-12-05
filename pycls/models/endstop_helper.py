efa8f79f1417dc9f278648dd77f809e14a40bdb4
Universe <bigtree123433@gmail.com>
2020-12-05 23:51:29 +0900

    sm 3x3

--- pycls/models/endstop_helper.py
+++ pycls/models/endstop_helper.py
@@ -445,16 +445,17 @@ class CompareFixedSM(nn.Conv2d):
         #                        [-0.23, 0.17, 0.49, 0.17, -0.23],
         #                        [-0.18, 0.49, 1, 0.49, -0.18],
         #                        [-0.23, 0.17, 0.49, 0.17, -0.23],
-        #                        [-0.27, -0.23, -0.18, -0.23, -0.27]], requires_grad=False).cuda
+        #                        [-0.27, -0.23, -0.18, -0.23, -0.27]], requires_grad=False).cuda()
         kernel = torch.tensor([[-1/8, -1/8, -1/8],
                               [-1/8, 1, -1/8],
-                              [-1/8, -1/8, -1/8]], requires_grad=False).cuda
-        kernel = kernel.repeat(out_channels, in_channels//groups, 1, 1)
+                              [-1/8, -1/8, -1/8]], requires_grad=False).cuda()
+        kernel = kernel.repeat((out_channels, in_channels//groups, 1, 1))
 
         return kernel
 
     def forward(self, x):
-        x = F.conv2d(x, self.param, stride=self.stride, padding=self.padding, groups=self.groups)
+        # x = F.conv2d(x, self.param, stride=self.stride, padding=self.padding, groups=self.groups)
+        x = F.conv2d(x, self.param, stride=self.stride, padding=(1, 1), groups=self.groups)
         return x
 
 
