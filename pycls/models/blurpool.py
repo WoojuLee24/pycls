08efcfff1f7976902a70e8d4d05452a117170533
Universe <bigtree123433@gmail.com>
2021-01-12 16:44:48 +0900

    param

--- pycls/models/blurpool.py
+++ pycls/models/blurpool.py
@@ -473,5 +473,5 @@ class SigmaCenterNormBlurPool5x5(nn.Conv2d):
         weight = self.get_weight(self.param)
         weight_norm = self.normalize_weight(weight)
         x = self.reflection_pad(x)
-        x = F.conv2d(x, weight stride=self.stride, groups=self.groups)
+        x = F.conv2d(x, weight, stride=self.stride, groups=self.groups)
         return x
