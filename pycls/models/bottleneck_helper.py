9562cc0687ffec48d9fa32a2d45bfc449af44254
Universe <bigtree123433@gmail.com>
2021-01-21 17:55:32 +0900

    param groups

--- pycls/models/bottleneck_helper.py
+++ pycls/models/bottleneck_helper.py
@@ -345,7 +345,7 @@ class ParamMaxBlurPoolTransform(Module):
             self.a = conv2d(w_in, w_out, 3, stride=1)
             self.a_bn = norm2d(w_out)
             self.a_af = activation()
-            self.max_blur = ParamBlurPool(w_out, w_out, stride=stride)
+            self.max_blur = ParamBlurPool(w_out, w_out, stride=stride, groups=w_out)
         else:
             self.a = conv2d(w_in, w_out, 3, stride=stride)
             self.a_bn = norm2d(w_out)
